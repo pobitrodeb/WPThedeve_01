fb8555f87d2e308a6c3cde4adeae6ae689f8a4a2
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-09-15 05:52:00 +0600

    Blog Title & Description Setup

--- wp-content/themes/keyapth/index.php
+++ wp-content/themes/keyapth/index.php
@@ -1,1 +1,16 @@
-<h1> <?php echo " Congratulations! keyapth theme is active now"; ?> </h1>
+<?php 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); ?> 
+
+        <!-- <?php the_title('<h1>', '</h1>'); ?>  -->
+        <h2> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h2>
+        <!-- <p> <?php the_content(); ?> </p> -->
+        <!-- <p> <?php the_excerpt(); ?> </p> -->
+        <?php
+		//
+		// Post Content here
+		//
+	} // end while
+} // end if
+?>
