8e2c53aca32075af8861ba6fcaca6f987ba76048
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-09-15 06:58:16 +0600

    Bootstrap CDN added

--- wp-content/themes/keyapth/header.php
+++ wp-content/themes/keyapth/header.php
@@ -6,6 +6,7 @@
     <link rel="profile" type="" href="https://gnpg.org/xfn/11">
     <title><?php wp_title('|', true, 'right'); ?> </title>
     <?php wp_head(); ?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body class="<?php body_class(); ?>">
     <h1><?php bloginfo('name');?></h1>
