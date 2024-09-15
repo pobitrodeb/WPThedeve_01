e57e7e60f15dad6526df503df28f428a70ffaa47
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-09-15 07:21:18 +0600

    Bootstrap Connect Local

--- wp-content/themes/keyapth/header.php
+++ wp-content/themes/keyapth/header.php
@@ -6,7 +6,12 @@
     <link rel="profile" type="" href="https://gnpg.org/xfn/11">
     <title><?php wp_title('|', true, 'right'); ?> </title>
     <?php wp_head(); ?>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Bootstrap Start  -->
+   <link rel="stylesheet" type="text/css" a href=" <?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" a href=" <?php echo get_template_directory_uri(); ?>/css/bootstrap-grid.min.css">
+   <link rel="stylesheet" type="text/css" a href=" <?php echo get_template_directory_uri(); ?>/css/bootstrap-reboot.min.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+    <!-- Bootstrap End  -->
 </head>
 <body class="<?php body_class(); ?>">
     <h1><?php bloginfo('name');?></h1>
