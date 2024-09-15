434d191c162df8b252ed0364d321ae93447384bb
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-09-15 06:36:27 +0600

    Header file added, title added,

---
+++ wp-content/themes/keyapth/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?> >
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" type="" href="https://gnpg.org/xfn/11">
+    <title><?php wp_title('|', true, 'right'); ?> </title>
+    <?php wp_head(); ?>
+</head>
+<body class="<?php body_class(); ?>">
+    
+    <section class="bodoy_control_keyapth">
+
+    </section>
+</body>
+</html>
--- wp-content/themes/keyapth/index.php
+++ wp-content/themes/keyapth/index.php
@@ -1,11 +1,12 @@
 <?php 
+    get_header(); 
 if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post(); ?> 
 
         <!-- <?php the_title('<h1>', '</h1>'); ?>  -->
         <h2> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h2>
-        <!-- <p> <?php the_content(); ?> </p> -->
+        <p> <?php the_content(); ?> </p>
         <!-- <p> <?php the_excerpt(); ?> </p> -->
         <?php
 		//
